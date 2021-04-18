4f4357f601ef5e11476002bbef37abbf18298d85
kavin1521 <79982621+kavin1521@users.noreply.github.com>
2021-04-18 16:35:10 +0530

    Add files via upload

---
+++ PRO-C-32-Template-main/PRO-C-32-Template-main/sketch.js
@@ -0,0 +1,86 @@
+const Engine = Matter.Engine;
+const World= Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+
+var engine, world;
+var backgroundImg;
+
+var bg ;
+
+function preload() {
+   getBackgroundImg();
+}
+
+function setup(){
+    var canvas = createCanvas(1200,700);
+    engine = Engine.create();
+    world = engine.world;
+
+}
+
+function draw(){
+    if(backgroundImg)
+    background(backgroundImg);
+
+    Engine.update(engine);
+
+}
+
+async function getBackgroundImg(){
+
+    var response = await fetch("https://worldtimeapi.org/api/timezone/Asia/Kolkata")
+    var responsejson = await response.json();
+    var datetime = responsejson.datetime;
+    var hour = datetime.slice(11,13);
+    if(hour >=06 && hour <=07){
+        bg="sunrise1.png"
+     }
+
+     if(hour >=07 && hour <=08){
+        bg="sunrise2.png"
+     }
+
+     if(hour >=08 && hour <=09){
+        bg="sunrise3.png"
+     }
+
+     if(hour >=09 && hour <=10){
+        bg="sunrise4.png"
+     }
+
+     if(hour >=11 && hour <=12){
+        bg="sunrise5.png"
+     }
+
+     if(hour >=13 && hour <=14){
+        bg="sunrise6.png"
+     }
+
+     if(hour >=15 && hour <=16){
+        bg="sunset7.png"
+     }
+
+     if(hour >=16 && hour <=17){
+        bg="sunset8.png"
+     }
+
+     if(hour >=17 && hour <=18){
+        bg="sunset9.png"
+     }
+
+     if(hour >=18 && hour <=19){
+        bg="sunset10.png"
+     }
+
+     if(hour >=19 && hour <=20){
+        bg="sunset11.png"
+     }
+
+     if(hour >=20 && hour <=21){
+        bg="sunset12.png"
+     }
+
+    backgroundImg=loadImage(bg);
+
+}
